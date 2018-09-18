80789b0b2c4469b7b53c69aa305b737b9375bb5b
Tomasz Borychowski <tomasz.borychowski@sap.com>
2018-09-18 15:15:44 +0100

    slim down & remove node backend

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,24 @@
 const gulp = require('gulp');
-const cssmin = require('gulp-clean-css');
-const concat = require('gulp-concat');
-const stylus = require('gulp-stylus');
-const eslint = require('gulp-eslint');
-const nodemon = require('gulp-nodemon');
-const noop = require('through2').obj;
-const sourcemaps = require('gulp-sourcemaps');
 const livereload = require('gulp-livereload');
-const env = require('gulp-env');
 const isProd = require('minimist')(process.argv.slice(2)).prod;
-const webpackConfig = require('./webpack.config.js');
-const webpack = require('webpack');
-const webpackStream = require('webpack-stream');
-const PUBLIC_PATH = 'public/';
-const chalk = require('chalk');
 
+const SRC_PATH = './src/';
+const PUBLIC_PATH = './public/';
 
-env.set({ NODE_ENV: isProd ? 'production' : 'development' });
 
 function webpackLogger (err) {
-	const time = new Date().toTimeString().substr(0,8);
-	let type = chalk.green('info'), message = 'webpack build finished';
-	if (err) {
-		type = chalk.red('error');
-		message = chalk.red(err);
-	}
-	console.log(`[${time}] [${type}] ${message}`);
+	const chalk = require('chalk');
+	let time = new Date().toTimeString().substr(0,8);
+	let message = 'Finished ' + chalk.green('webpack') + ' build';
+	if (err) { message = chalk.red(err); time = chalk.red(time); }
+	else time = chalk.grey(time);
+	console.log(`[${time}] ${message}`); /* eslint no-console: 0 */
 }
-
-gulp.task('help', () => {
-	const tasks = '  ' + Object.keys(gulp.tasks).sort().join('\n  ');
-	console.log(`\nAvailable tasks:\n${tasks}\n`);/* eslint no-console: 0 */
-});
 
 
 gulp.task('eslint', () => {
-	return gulp.src(['client/**/*.js', 'server/**/*.js'])
+	const eslint = require('gulp-eslint');
+	return gulp.src([SRC_PATH + '**/*.js'])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failAfterError());
@@ -43,25 +26,33 @@ gulp.task('eslint', () => {
 
 
 gulp.task('assets', () => {
-	gulp.src(['assets/*.*']).pipe(gulp.dest(`${PUBLIC_PATH}`));
+	gulp.src(['assets/*.*']).pipe(gulp.dest(PUBLIC_PATH));
 });
 
 
 gulp.task('js', ['eslint'], () => {
-	if (!isProd) {
-		webpackConfig.devtool = 'inline-source-map';
-		webpackConfig.mode = 'development';
-	}
-	else {
-		const MinifyPlugin = require('babel-minify-webpack-plugin');
-		webpackConfig.plugins = [ new MinifyPlugin() ];
-	}
+	const webpack = require('webpack');
+	const webpackStream = require('webpack-stream');
+	const path = require('path');
+	const MinifyPlugin = require('babel-minify-webpack-plugin');
 
-	return gulp.src(['client/index.js'])
+	const webpackConfig = {
+		devtool: isProd ? undefined : 'inline-source-map',
+		mode: isProd ? 'production' : 'development',
+		entry: { index: SRC_PATH + 'index.js' },
+		output: { filename: 'app.js', path: path.join(__dirname, 'public'), publicPath: PUBLIC_PATH },
+		resolve: { extensions: ['.js', '.json', '.html'] },
+		module: { rules: [
+			{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+			{ test: /\.html$/, exclude: /node_modules/, use: { loader: 'svelte-loader', options: { css: false }}},
+		]},
+		plugins: isProd ? [ new MinifyPlugin() ] : []
+	};
+
+
+	return gulp.src([SRC_PATH + 'index.js'])
 		.pipe(webpackStream(webpackConfig, webpack, webpackLogger))
-		.on('error', function () {
-			this.emit('end');
-		})
+		.on('error', function () { this.emit('end'); })
 		.pipe(gulp.dest(PUBLIC_PATH))
 		.pipe(livereload());
 
@@ -69,9 +60,15 @@ gulp.task('js', ['eslint'], () => {
 
 
 gulp.task('styl', () => {
-	return gulp.src(['client/index.styl', 'client/**/*.styl'])
+	const sourcemaps = require('gulp-sourcemaps');
+	const cssmin = require('gulp-clean-css');
+	const concat = require('gulp-concat');
+	const stylus = require('gulp-stylus');
+	const noop = require('through2').obj;
+
+	return gulp.src([SRC_PATH + 'index.styl', SRC_PATH + '**/*.styl'])
 		.pipe(isProd ? noop() : sourcemaps.init())
-		.pipe(stylus({ paths: ['client'], 'include css': true }))
+		.pipe(stylus({ paths: [SRC_PATH], 'include css': true }))
 		.pipe(isProd ? cssmin({ keepSpecialComments: 0 }) : noop())
 		.pipe(concat('app.css'))
 		.pipe(isProd ? noop() : sourcemaps.write())
@@ -80,25 +77,11 @@ gulp.task('styl', () => {
 });
 
 
-let serverStarted = false;
-gulp.task('server', done => {
-	return nodemon({ script: './server/index.js', watch: ['./server'], ext: 'js html' })
-		.on('start', () => {
-			if (serverStarted) return;
-			serverStarted = true;
-			setTimeout(done, 500);
-		});
-});
-
-
-gulp.task('watch', ['default'], () => {
+gulp.task('default', [ 'assets', 'styl', 'js' ], () => {
 	if (isProd) return;
 	livereload.listen();
-	gulp.watch('client/**/*.styl', ['styl']);
-	gulp.watch('client/**/*.js', ['js']);
-	gulp.watch('client/**/*.html', ['js']);
+	gulp.watch(SRC_PATH + '**/*.styl', ['styl']);
+	gulp.watch(SRC_PATH + '**/*.js', ['js']);
+	gulp.watch(SRC_PATH + '**/*.html', ['js']);
 	gulp.watch('assets/**/*.*', ['assets']);
 });
-
-
-gulp.task('default', [ 'js', 'styl', 'assets' ]);
